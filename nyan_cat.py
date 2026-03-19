13ef5f1f93c3d4568227ba8f3811edb75548b277
Pika <pika.tomazin@gmail.com>
2026-03-19 18:46:12 +0100

    start

--- nyan_cat.py
+++ nyan_cat.py
@@ -4,9 +4,11 @@ py.init()
 canvas = py.display.set_mode((500,500))
 py.display.set_caption('nyan_cat')
 
+color = (200,0,200)
 ext = False
 
 while not ext:
+    canvas.fill(color)
     for event in py.event.get():
         if event.type == py.QUIT:
             ext = True
