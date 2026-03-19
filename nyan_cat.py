7c8caa927436b3361442ead039cbe9e983f1f4a4
Pika <pika.tomazin@gmail.com>
2026-03-19 18:38:07 +0100

    ogrodje

--- nyan_cat.py
+++ nyan_cat.py
@@ -3,3 +3,10 @@ import pygame as py
 py.init()
 canvas = py.display.set_mode((500,500))
 py.display.set_caption('nyan_cat')
+
+ext = False
+
+while not ext:
+    for event in py.event.get():
+        if event.type == py.QUIT:
+            ext = True
