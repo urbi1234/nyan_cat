7aa7c31cad7e906736fd7581d5e86337b75d2cff
Pika <pika.tomazin@gmail.com>
2026-03-26 19:35:34 +0100

    gravitacija

--- nyan_cat.py
+++ nyan_cat.py
@@ -14,6 +14,9 @@ y = 10
 clock = py.time.Clock()
 player = py.Rect(x,y,30,30)
 
+gravitacija = 3
+hitrost_gor = 0
+
 while not ext:
     clock.tick(30)
     canvas.fill(color)
@@ -23,10 +26,15 @@ while not ext:
 
     keys = py.key.get_pressed()
 
-    if keys[py.K_SPACE]:
-        player.y -= 10
-        
-        player.y += 10
+    if keys[py.K_SPACE] and hitrost_gor < 2:
+        hitrost_gor = 15
+
+
+    #padanje
+    player.y += gravitacija - hitrost_gor
+
+    if hitrost_gor > 0:
+        hitrost_gor -= 1
 
 
     py.draw.rect(canvas, (10, 230, 180), player)
